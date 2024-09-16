6829f66269f843eb33a6ae7496dff19f9f51f8a4
spcymshrm <joe.jordan1994@gmail.com>
2024-09-16 14:36:15 -0400

    Created variables for buttons
    Created const variables to assign to each button on the calculator

---
+++ Code/script.js
@@ -0,0 +1,23 @@
+const clearButton = document.querySelector("#clear");
+const signButton = document.querySelector("#sign");
+const percentButton = document.querySelector("#percent");
+const divideButton = document.querySelector("#divide");
+
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const multiplyButton = document.querySelector("#multiply");
+
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const minusButton = document.querySelector("#minus");
+
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const plusButton = document.querySelector("#plus");
+
+const zeroButton = document.querySelector("#zero");
+const decimalButton = document.querySelector("#decimal");
+const equalsButton = document.querySelector("#equals");
